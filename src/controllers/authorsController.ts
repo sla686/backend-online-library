73737f1363a8ae4c49ba4e7f30f749ee1c17c1eb
Viacheslav Semushin <sla686@gmail.com>
2022-07-13 22:45:42 +0300

    added path for authors

---
+++ src/controllers/authorsController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { CustomError } from "ErrorType";
+
+const getAllAuthors = (req: Request, res: Response) => {
+  return res.send("Authors list");
+};
+
+const getSingleAuthor = (req: Request, res: Response) => {
+  const { authorId } = req.params;
+  if (authorId === "1") {
+    throw new CustomError(401, "Author not found");
+  }
+  return res.send(`Author id is ${authorId}`);
+};
+
+export default { getAllAuthors, getSingleAuthor };
