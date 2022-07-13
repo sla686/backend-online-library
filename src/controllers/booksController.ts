b7015d94134955f9df47c39e0ab752cdbf48d389
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-07-13 18:33:20 +0300

    added types folder with errorType.ts, and getSinglebook in bookscontroller

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
+import { CustomError } from "ErrorType";
 
 const getAllBooks = (req: Request, res: Response) => {
   return res.send("response: books list");
 };
 
-// const getSingleBooks = (req: Request, res: Response) => {
-//     const { booksId } = req.params
-//     if (bookId === "1") {
-//         throw new CustomError(401, 'Not user found')
-//     }
-//     return res.send(`response:${bookId}`)
-// }
+const getSingleBook = (req: Request, res: Response) => {
+  const { bookId } = req.params;
+  if (bookId === "1") {
+    throw new CustomError(401, "Not user found");
+  }
+  return res.send(`response:${bookId}`);
+};
 
-export default { getAllBooks };
+export default { getAllBooks, getSingleBook };
---
+++ src/types/ErrorType.ts
@@ -0,0 +1,5 @@
+export class CustomError extends Error {
+  constructor(readonly status: number, readonly message: string) {
+    super();
+  }
+}
