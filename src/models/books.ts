d026fdf8e8c457ae046f311fbe01f1df93699a2d
Viacheslav Semushin <sla686@gmail.com>
2022-07-19 20:28:50 +0300

    Prettified the code

--- src/models/books.ts
+++ src/models/books.ts
@@ -1,18 +1,42 @@
 import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
+// Confusing! Use modern ES destructuring as follows:
+const { Schema } = mongoose;
 
+// Or you can also just type "mongoose.Schema"
 const bookSchema = new Schema({
-  isbn: { String, required: true },
-  title: { String, required: true },
-  description: { String, required: true },
-  publisher: { String, required: true },
-  authors: { String, required: true },
-  status: { String, required: true },
-  published_date: { String, required: true },
-  borrower_id: { String },
-  borrow_date: { String },
-  return_date: { String },
+  isbn: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  published_date: {
+    type: String,
+    required: true,
+  },
+  borrower_id: String,
+  borrow_date: String,
+  return_date: String,
 });
 
 const Book = mongoose.model("book", bookSchema);
