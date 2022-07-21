2b157e0784e585a4a39cd6a44a6989a0cff5ec3e
Amrit618 <amritbastakoti618@gmail.com>
2022-07-21 20:53:56 +0300

    error getting all users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from "express";
+import User from "../models/Users";
 
-const getAllUsers = (req: Request, res: Response) => {
-  return res.send("FIND Here all users list!!!");
+const getAllUsers = async (req: Request, res: Response) => {
+  const user = await User.find();
+  res.json(user);
 };
-
+const createUser = async (req: Request, res: Response) => {
+  const { firstname, lastname, email, role } = req.body;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const newUser = await User.create({
+    firstname,
+    lastname,
+    email,
+    role
+    
+  }).then((data) => {
+    res.json(data);
+  });
+};
 
 const getSingleUser = (req: Request, res: Response) => {
   return res.send(`response:${req.params.userId}`);
-  
 };
 
 const postSingleUser = (req: Request, res: Response) => {
-return res.send("add a user")
+  return res.send("add a user");
 };
 
 const patchSingleUser = (req: Request, res: Response) => {
@@ -23,8 +36,10 @@ const deleteSingleUser = (req: Request, res: Response) => {
 };
 
 export default {
-    getAllUsers,
-    getSingleUser,
-    patchSingleUser,
-    postSingleUser,
-    deleteSingleUser };
+  getAllUsers,
+  createUser,
+  getSingleUser,
+  patchSingleUser,
+  postSingleUser,
+  deleteSingleUser,
+};
