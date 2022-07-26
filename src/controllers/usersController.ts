c49f5db5757a0c7ece2ea94b5b502da303b39ad1
Amrit618 <amritbastakoti618@gmail.com>
2022-07-26 21:25:56 +0300

    verified credintials

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -88,55 +88,28 @@ const deleteSingleUser = async (
   }
 };
 
+const verifyCredential = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  const foundUser = await User.findOne({ email: email });
+  if (foundUser) {
+    const checkPassword = await foundUser.comparePassword(password);
+    if (checkPassword) {
+      return res.json(foundUser);
+    }
+  } else {
+    throw new CustomError(401, "email and pw not in use");
+  }
+};
+
 export default {
   getAllUsers,
   postUsers,
   getSingleUser,
   patchSingleUser,
   deleteSingleUser,
+  verifyCredential
 };
-// import { Request, Response } from "express";
-// import User from "../models/Users";
-
-// const getAllUsers = async (req: Request, res: Response) => {
-//   const user = await User.find();
-//   res.json(user);
-// };
-// const createUser = async (req: Request, res: Response) => {
-//   const { firstname, lastname, email, role } = req.body;
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-//   const newUser = await User.create({
-//     firstname,
-//     lastname,
-//     email,
-//     role
-
-//   }).then((data) => {
-//     res.json(data);
-//   });
-// };
-
-// const getSingleUser = (req: Request, res: Response) => {
-//   return res.send(`response:${req.params.userId}`);
-// };
-
-// const postSingleUser = (req: Request, res: Response) => {
-//   return res.send("add a user");
-// };
-
-// const patchSingleUser = (req: Request, res: Response) => {
-//   return res.send(`updates:${req.params.userId}`);
-// };
-
-// const deleteSingleUser = (req: Request, res: Response) => {
-//   return res.send(`delete:${req.params.userId}`);
-// };
-
-// export default {
-//   getAllUsers,
-//   createUser,
-//   getSingleUser,
-//   patchSingleUser,
-//   postSingleUser,
-//   deleteSingleUser,
-// };
